7055ff5cad11e844e002dee42b67dbbeae151e01
@supportIpetik <support@ipetik.com>
2024-02-20 21:10:02 -0600

    modificacion conexion db

--- Db/config.js
+++ Db/config.js
@@ -4,4 +4,4 @@
  export const DB_USER = process.env.DB_USER || 'root'
  export const DB_PASSWORD = process.env.DB_PASSWORD || '4Ee6hcD15fBEec4aaABF2bHgca1fG5a6'
  export const DB_NAME = process.env.DB_NAME || 'railway'
- export const DB_PORT = process.env.DB_PORT || 6004
+ export const DB_PORT = process.env.DB_PORT || 25668
