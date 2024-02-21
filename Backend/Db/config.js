16a7f3fbdce33ddc49e447e7d8808c34b79bb886
@supportIpetik <support@ipetik.com>
2024-02-20 20:16:39 -0600

    CAMBIOS

--- Backend/Db/config.js
+++ Backend/Db/config.js
@@ -1,7 +1,7 @@
  export const PORT = process.env.PORT || 3001
 
- export const DB_HOST = process.env.DB_HOST || 'containers-us-west-147.railway.app'
+ export const DB_HOST = process.env.DB_HOST || 'monorail.proxy.rlwy.net'
  export const DB_USER = process.env.DB_USER || 'root'
- export const DB_PASSWORD = process.env.DB_PASSWORD || 'vqlTp0w4DW2hemkEnZEK'
+ export const DB_PASSWORD = process.env.DB_PASSWORD || '4Ee6hcD15fBEec4aaABF2bHgca1fG5a6'
  export const DB_NAME = process.env.DB_NAME || 'railway'
  export const DB_PORT = process.env.DB_PORT || 6004
